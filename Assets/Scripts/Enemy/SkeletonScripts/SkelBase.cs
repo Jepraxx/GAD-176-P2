774807bfd4d4fc13463baf7af51a6b121f8bc1a9
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2024-08-05 09:44:32 +0400

    Creation of the base script for the skeleton

--- Assets/Scripts/Enemy/SkeletonScripts/SkelBase.cs
+++ Assets/Scripts/Enemy/SkeletonScripts/SkelBase.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script is the base skeleton script and all the
+/// other skeletons would inherit from this script
+/// </summary>
+
 public class SkelBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] protected float moveSpeed = 0.5f;
+    [SerializeField] protected float distance = 20f;
+
+    public Transform target;
+
+    // At the start, find the player
+    protected virtual void Start()
     {
-        
+        FindPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Find the player
+    protected virtual void FindPlayer()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Transform>();
+        if (target == null)
+        {
+            Debug.Log("Player not found! Make sure the player has the 'Player' tag.");
+        }
+    }
+
+    private void Update()
+    {
+        Move(); // Call the move function
+    }
+
+    // This function is responsible for the tracking system of the skeletons, other skeletons would also inherit this function but can override it to have different behaviors.
+    protected virtual void Move()
+    {
+        if (target != null && Vector2.Distance(transform.position, target.position) < distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+        else if (target == null)
+        {
+            Debug.Log("Target is null. Ensure the player is tagged correctly.");
+        }
     }
 }
