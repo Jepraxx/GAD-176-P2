ffcf2e7b801db68868691d07fe30bf69b816648d
Mattias Pothen Matthew <pothen.mattias@student.saedubai.com>
2024-08-06 11:37:02 +0400

    BUG fixes and New Script
    Did some bug fixes and inculded a script for my scritableobject

--- Assets/Scripts/Player/PlayerSkillManager.cs
+++ Assets/Scripts/Player/PlayerSkillManager.cs
@@ -3,39 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PlayerSkillManager : MonoBehaviour
+namespace _scripts.Skill_system
 {
-    private int _strength, _dexterity, _intelligence, _magic, _stamina; //stats for the player
-    private int _doubleJump, _dash, _fireBall, _invisiblity; //unlocables for the player
-    private int _skillPoints;
-
-    public int Strength => _strength;
-    public int Dexterity => _dexterity;
-    public int Intelligence => _intelligence;
-    public int Magic => _magic;
-    public int Stamina => _stamina;
-
-    public bool DoubleJump => _doubleJump > 0;
-    public bool Dash => _dash > 0;
-    public bool FireBall => _fireBall > 0;
-    public bool Invisibility => _invisiblity > 0;
-    public int SkillPoints => _skillPoints;
-
-    public UnityAction OnSkillPointsChanged;
-    private void Awake()
+    public class PlayerSkillManager : MonoBehaviour
     {
-        _skillPoints = 10;
-        _intelligence = 10;
-        _magic = 10;
-        _stamina = 10;
-        _dexterity = 10;
-        _strength = 10;
+        private int _strength, _dexterity, _intelligence, _magic, _stamina; // stats for the player
+        private int _doubleJump, _dash, _fireBall, _invisibility; // unlockables for the player
+        private int _skillPoints;
 
-    }
+        public int Strength => _strength;
+        public int Dexterity => _dexterity;
+        public int Intelligence => _intelligence;
+        public int Magic => _magic;
+        public int Stamina => _stamina;
 
-    public void GainSkillPoint()
-    {
-        _skillPoints++;
-        OnSkillPointsChanged?.Invoke();
+        public bool DoubleJump => _doubleJump > 0;
+        public bool Dash => _dash > 0;
+        public bool FireBall => _fireBall > 0;
+        public bool Invisibility => _invisibility > 0;
+        public int SkillPoints => _skillPoints;
+
+        public UnityAction OnSkillPointsChanged;
+
+        private void Awake()
+        {
+            _skillPoints = 10;
+            _intelligence = 10;
+            _magic = 10;
+            _stamina = 10;
+            _dexterity = 10;
+            _strength = 10;
+        }
+
+        public void GainSkillPoint()
+        {
+            _skillPoints++;
+            OnSkillPointsChanged?.Invoke();
+        }
     }
 }
---
+++ Assets/Scripts/Skill/ScriptableSkill.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Skill_System
+{
+    [CreateAssetMenu(fileName = "S", menuName = "MENUNAME", order = 0)]
+    public class ScriptableSkill : ScriptableObject
+    {
+        // Define properties and methods specific to the ScriptableSkill here
+    }
+}
