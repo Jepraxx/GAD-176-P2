a9d49854a15dc01fc5c82a6f87cf400b4351a024
Mattias Pothen Matthew <pothen.mattias@student.saedubai.com>
2024-08-06 13:41:43 +0400

    error in codes

--- Assets/Scripts/Player/PlayerSkillManager.cs
+++ Assets/Scripts/Player/PlayerSkillManager.cs
@@ -3,42 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace _scripts.Skill_system
+public class PlayerSkillManager : MonoBehaviour
 {
-    public class PlayerSkillManager : MonoBehaviour
+    private int _strength, _dexterity, _intelligence, _magic, _stamina; //stats for the player
+    private int _doubleJump, _dash, _fireBall, _invisiblity; //unlocables for the player
+    private int _skillPoints;
+
+    public int Strength => _strength;
+    public int Dexterity => _dexterity;
+    public int Intelligence => _intelligence;
+    public int Magic => _magic;
+    public int Stamina => _stamina;
+
+    public bool DoubleJump => _doubleJump > 0;
+    public bool Dash => _dash > 0;
+    public bool FireBall => _fireBall > 0;
+    public bool Invisibility => _invisiblity > 0;
+    public int SkillPoints => _skillPoints;
+
+    public UnityAction OnSkillPointsChanged;
+    private void Awake()
     {
-        private int _strength, _dexterity, _intelligence, _magic, _stamina; // stats for the player
-        private int _doubleJump, _dash, _fireBall, _invisibility; // unlockables for the player
-        private int _skillPoints;
+        _skillPoints = 10;
+        _intelligence = 10;
+        _magic = 10;
+        _stamina = 10;
+        _dexterity = 10;
+        _strength = 10;
 
-        public int Strength => _strength;
-        public int Dexterity => _dexterity;
-        public int Intelligence => _intelligence;
-        public int Magic => _magic;
-        public int Stamina => _stamina;
+    }
 
-        public bool DoubleJump => _doubleJump > 0;
-        public bool Dash => _dash > 0;
-        public bool FireBall => _fireBall > 0;
-        public bool Invisibility => _invisibility > 0;
-        public int SkillPoints => _skillPoints;
-
-        public UnityAction OnSkillPointsChanged;
-
-        private void Awake()
-        {
-            _skillPoints = 10;
-            _intelligence = 10;
-            _magic = 10;
-            _stamina = 10;
-            _dexterity = 10;
-            _strength = 10;
-        }
-
-        public void GainSkillPoint()
-        {
-            _skillPoints++;
-            OnSkillPointsChanged?.Invoke();
-        }
+    public void GainSkillPoint()
+    {
+        _skillPoints++;
+        OnSkillPointsChanged?.Invoke();
     }
 }
